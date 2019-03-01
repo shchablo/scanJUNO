65b5459bb72e6e1627c6215924428230bd844ee3
shchablo <shchabloKV@gmail.com>
2019-03-01 13:01:02 +0100

    Update simple_server.c

--- qsys/cpu_eth/software/eth/simple_server.c
+++ qsys/cpu_eth/software/eth/simple_server.c
@@ -1,19 +1,5 @@
 /*******************************************************************************
 * Copyright [2016] [Guido Socher (GPL V2), Shchablo Konstantin]
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-* either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
 
 /*******************************************************************************
 * Information.
@@ -1398,8 +1384,8 @@ int simple_server()
                    strcpy(str, "Ok\n");
                    goto ANSWER;
                    }
-                   if((strncmp("Конец", (char *) &(buf[i]), 5) == 0)) {	// parsing dac data
-					 strcpy(str, "Спасибо за внимание!\n");
+                   if((strncmp("ГЉГ®Г­ГҐГ¶", (char *) &(buf[i]), 5) == 0)) {	// parsing dac data
+					 strcpy(str, "Г‘ГЇГ Г±ГЁГЎГ® Г§Г  ГўГ­ГЁГ¬Г Г­ГЁГҐ!\n");
 					 goto ANSWER;
                    }
 
